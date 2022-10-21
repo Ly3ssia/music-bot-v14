34ed620ea53021b2d9d3c28e86c3193d759c0dae
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-10-22 00:16:03 +0300

    Add files via upload

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,5 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-    let activities = [ `Developed by memte#7601`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
+  client.user.setActivity("🎵 | Raven Music")
 }};
