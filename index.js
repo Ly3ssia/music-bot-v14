0608f5a9e349a18129f22085b05d8e4c5dbb16bc
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-10-22 00:28:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
-const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js")
+const Discord = require("discord.js")
+const client = new Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.MessageContent
+  ]
+})
 const config = require("./src/config.js");
 const { readdirSync } = require("fs")
 const moment = require("moment");
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
+const { DisTube } = require('distube')
+const { SpotifyPlugin } = require('@distube/spotify')
+const { SoundCloudPlugin } = require('@distube/soundcloud')
+const { YtDlpPlugin } = require('@distube/yt-dlp')
+const { Player } = require("discord-player")
 const db = require("croxydb")
+const languagefile = require("./src/language.json")
+const player = new Player(client);
+client.player = player;
+client.distube = new DisTube(client, {
+  leaveOnStop: false,
+  leaveOnFinish: true,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+  plugins: [
+    new SpotifyPlugin({
+      emitEventsAfterFetching: true
+    }),
+    new SoundCloudPlugin(),
+    new YtDlpPlugin()
+  ]
+})
 let token = config.token
 
 client.commands = new Collection()
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-const log = l => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${l}`) };
-
-//command-handler
 const commands = [];
 readdirSync('./src/commands').forEach(async file => {
   const command = require(`./src/commands/${file}`);
   commands.push(command.data.toJSON());
   client.commands.set(command.data.name, command);
 })
+client.distube.on("finish", queue => {
+  client.guilds.cache.filter(guild => {
+const data = db.fetch(`music_${guild.id}`)
+if (!data) return;
+const mesaj = data.mesaj
+const channels = data.kanal
+const channel = guild.channels.cache.get(channels)
+const messagef = channel.messages.fetch(mesaj).then(async messagef => {
+messagef.edit({content: "🎵 | Music ended.", embeds: [], components: []}).catch(err => {})
+})
+})
+})
 
+client.on("ready", async () => {
+  client.guilds.cache.filter(guild => {
+const data = db.fetch(`music_${guild.id}`)
+if (!data) return;
+db.delete(`music_${guild.id}`)
+})
+})
 client.on("ready", async () => {
         try {
             await rest.put(
@@ -31,10 +77,8 @@ client.on("ready", async () => {
         } catch (error) {
             console.error(error);
         }
-    log(`${client.user.username} Aktif Edildi!`);
+    console.log(`Bot logged in as ${client.user.tag}!`);
 })
-
-//event-handler
 readdirSync('./src/events').forEach(async file => {
 	const event = require(`./src/events/${file}`);
 	if (event.once) {
@@ -43,122 +87,223 @@ readdirSync('./src/events').forEach(async file => {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 })
-//
 
-client.login(token)
+client.on("interactionCreate", interaction => {
+  if (interaction.customId === "fast") {
+    const queue = client.distube.getQueue(interaction);
+       if (!queue) return interaction.reply(`There is no song on the list yet.`)
+    let data = db.fetch(`music_${interaction.guild.id}`)
+    if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+    queue.filters.add("nightcore")
+interaction.reply({content: "Speed online!", ephemeral: true})
+  }
+  if (interaction.customId === "slowmode") {
+    const queue = client.distube.getQueue(interaction);
+       if (!queue) return interaction.reply(`There is no song on the list yet.`)
+    let data = db.fetch(`music_${interaction.guild.id}`)
+    if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+    queue.filters.add("vaporwave")
+interaction.reply({content: "Slowmode online!", ephemeral: true})
+  }
+  if (interaction.customId === "bassboost") {
+    const queue = client.distube.getQueue(interaction);
+       if (!queue) return interaction.reply(`There is no song on the list yet.`)
+    let data = db.fetch(`music_${interaction.guild.id}`)
+    if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+    queue.filters.add("bassboost")
+interaction.reply({content: "Bassboost online!", ephemeral: true})
+  }
+  if (interaction.customId === "soru") {
+    const queue = client.distube.getQueue(interaction);
+       if (!queue) return interaction.reply(`There is no song on the list yet.`)
+    let data = db.fetch(`music_${interaction.guild.id}`)
+    if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+    const part = Math.floor((queue.currentTime / queue.songs[0].duration) * 20);
+    const embed = new Discord.EmbedBuilder()
+        .setColor('Purple')
+        .setDescription(`**[${queue.songs[0].name}](${queue.songs[0].url})**`)
+        .setImage(`${queue.songs[0].thumbnail}`)
+        .addFields({ name: 'Music Author:', value: `[${queue.songs[0].uploader.name}](${queue.songs[0].uploader.url})`, inline: true })
+        .addFields({ name: 'Member:', value: `${queue.songs[0].user}`, inline: true })
+        .addFields({ name: 'Voice:', value: `${queue.volume}%`, inline: true })
+        .addFields({ name: 'Views:', value: `${queue.songs[0].views}`, inline: true })
+        .addFields({ name: 'Like:', value: `${queue.songs[0].likes}`, inline: true })
+        .addFields({ name: 'Filtre:', value: `${queue.filters.names.join(', ') || "Normal"}`, inline: true })
+        .addFields({ name: `Video Time: **[${queue.formattedCurrentTime} / ${queue.songs[0].formattedDuration}]**`, value: ` ${'<:circle:1033057941647016056>'.repeat(part) + '🎵' + '<:asd:1033046466438107158>'.repeat(20 - part)}`, inline: false })
+return interaction.reply({embeds: [embed], ephemeral: true}).catch(err => {})
+  }
+if (interaction.customId === "dur") {
+  const queue = client.distube.getQueue(interaction);
+     if (!queue) return interaction.reply(`There is no song on the list yet.`)
+  let data = db.fetch(`music_${interaction.guild.id}`)
+  if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+  let usır = data.user
+  let string = data.string
+  if (interaction.user.id !== usır) return interaction.reply({content: "Only the person who wrote the command can use this button.", ephemeral: true})
+const baslik = data.başlık
+const author = data.yükleyen
+const sure = data.süre
+const izlenme = data.görüntülenme
+const thumb = data.thumb
+const url = data.video
+const embed = new Discord.EmbedBuilder()
+.addFields({name: "Title", value: `${baslik}`, inline: true})
+.addFields({name: "Author", value: `${author}`, inline: true})
+.addFields({name: "Time", value: `${sure}`, inline: true})
+.addFields({name: "Views", value: `${izlenme}`, inline: true})
+.addFields({name: "Thumbnail", value: "[Click]("+thumb+")", inline: true})
+.addFields({name: "Video", value: "[Click]("+url+")", inline: true})
+.setColor("Aqua")
+.setImage(`${thumb}`)
+const row = new Discord.ActionRowBuilder()
+.addComponents(
+new Discord.ButtonBuilder()
+.setEmoji("🎵")
+.setStyle(Discord.ButtonStyle.Danger)
+.setCustomId("devam")
+)
+client.distube.pause(interaction)
+return interaction.update({embeds: [embed], components: [row]})
+}
+if (interaction.customId === "skip") {
+  const queue = client.distube.getQueue(interaction);
+     if (!queue) return interaction.reply(`There is no song on the list yet.`)
+  let data = db.fetch(`music_${interaction.guild.id}`)
+  if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+    if (queue.songs.length === 1) return interaction.reply("No song found in the queue!")
+  let usır = data.user
+  let string = data.string
+  if (interaction.user.id !== usır) return interaction.reply({content: "Only the person who wrote the command can use this button.", ephemeral: true})
+const baslik = data.başlık
+const author = data.yükleyen
+const sure = data.süre
+const izlenme = data.görüntülenme
+const thumb = data.thumb
+const url = data.video
+const embed = new Discord.EmbedBuilder()
+.addFields({name: "Title", value: `${baslik}`, inline: true})
+.addFields({name: "Author", value: `${author}`, inline: true})
+.addFields({name: "Time", value: `${sure}`, inline: true})
+.addFields({name: "Views", value: `${izlenme}`, inline: true})
+.addFields({name: "Thumbnail", value: "[Click]("+thumb+")", inline: true})
+.addFields({name: "Video", value: "[Click]("+url+")", inline: true})
+.setColor("Aqua")
+.setImage(`${thumb}`)
 
+client.distube.skip(interaction)
+return interaction.update({embeds: [embed]})
+}
+if (interaction.customId === "loop") {
+  const queue = client.distube.getQueue(interaction);
+     if (!queue) return interaction.reply(`There is no song on the list yet.`)
+  let data = db.fetch(`music_${interaction.guild.id}`)
+  if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+  let usır = data.user
+  let string = data.string
+  if (interaction.user.id !== usır) return interaction.reply({content: "Only the person who wrote the command can use this button.", ephemeral: true})
+const baslik = data.başlık
+const author = data.yükleyen
+const sure = data.süre
+const izlenme = data.görüntülenme
+const thumb = data.thumb
+const url = data.video
+const embed = new Discord.EmbedBuilder()
+.addFields({name: "Title", value: `${baslik}`, inline: true})
+.addFields({name: "Author", value: `${author}`, inline: true})
+.addFields({name: "Time", value: `${sure}`, inline: true})
+.addFields({name: "Views", value: `${izlenme}`, inline: true})
+.addFields({name: "Thumbnail", value: "[Click]("+thumb+")", inline: true})
+.addFields({name: "Video", value: "[Click]("+url+")", inline: true})
+.setColor("Aqua")
+.setImage(`${thumb || "https://cdn.discordapp.com/attachments/997487955860009038/1009062859889705062/Baslksz-1.png"}`)
+client.distube.setRepeatMode(interaction, 1);
+return interaction.update({embeds: [embed]})
+}
+if (interaction.customId === "devam") {
+  const queue = client.distube.getQueue(interaction);
+     if (!queue) return interaction.reply(`There is no song on the list yet.`)
+  let data = db.fetch(`music_${interaction.guild.id}`)
+  if (!data) return interaction.reply({content: "I'm sorry.Error **404**", ephemeral: true})
+  let usır = data.user
+  let string = data.string
+  if (interaction.user.id !== usır) return interaction.reply({content: "Only the person who wrote the command can use this button.", ephemeral: true})
+  const baslik = data.başlık
+  const author = data.yükleyen
+  const sure = data.süre
+  const izlenme = data.görüntülenme
+  const thumb = data.thumb
+  const url = data.video
+  const embed = new Discord.EmbedBuilder()
+  .addFields({name: "Title", value: `${baslik}`, inline: true})
+  .addFields({name: "Author", value: `${author}`, inline: true})
+  .addFields({name: "Time", value: `${sure}`, inline: true})
+  .addFields({name: "Views", value: `${izlenme}`, inline: true})
+  .addFields({name: "Thumbnail", value: "[Click]("+thumb+")", inline: true})
+  .addFields({name: "Video", value: "[Click]("+url+")", inline: true})
+  .setColor("Aqua")
+  .setImage(`${thumb}`)
+  const row = new Discord.ActionRowBuilder()
+  .addComponents(
+  new Discord.ButtonBuilder()
+  .setEmoji("🎵")
+  .setStyle(Discord.ButtonStyle.Danger)
+  .setCustomId("dur"),
+  new Discord.ButtonBuilder()
+  .setEmoji("🔊")
+  .setStyle(Discord.ButtonStyle.Success)
+  .setCustomId("volume"),
+  new Discord.ButtonBuilder()
+  .setEmoji("⏩")
+  .setStyle(Discord.ButtonStyle.Primary)
+  .setCustomId("skip"),
+  new Discord.ButtonBuilder()
+  .setEmoji("🌀")
+  .setStyle(Discord.ButtonStyle.Secondary)
+  .setCustomId("loop"),
+  new Discord.ButtonBuilder()
+  .setLabel("Support Server")
+  .setStyle(Discord.ButtonStyle.Link)
+  .setURL("https://discord.gg/altyapilar")
+  )
+  client.distube.resume(interaction)
+  interaction.update({embeds: [embed], components: [row]})
+}
+})
 
-const Discord = require("discord.js")
 const modal = new Discord.ModalBuilder()
 .setCustomId('form')
-.setTitle('Code Share - Raven')
+.setTitle('Raven - Music Bot!')
   const a1 = new Discord.TextInputBuilder()
-  .setCustomId('baslik')
-  .setLabel('Kodunuzun İsmi')
+  .setCustomId('setvolume')
+  .setLabel('Volume')
   .setStyle(Discord.TextInputStyle.Paragraph)
-  .setMinLength(2)
-  .setPlaceholder('istatistik-v14')
+  .setMinLength(1)
+  .setPlaceholder('1 - 100')
   .setRequired(true)
-	const a2 = new Discord.TextInputBuilder()
-	.setCustomId('kod')
-	.setLabel('Kod')
-  .setStyle(Discord.TextInputStyle.Paragraph)
-	.setMinLength(1)
-	.setPlaceholder('Lütfen v14 altı kod paylaşmayınız.')
-	.setRequired(true)
 
     const row = new Discord.ActionRowBuilder().addComponents(a1);
-    const row2 = new Discord.ActionRowBuilder().addComponents(a2);
 
-    modal.addComponents(row, row2);
+    modal.addComponents(row);
 
 
 client.on('interactionCreate', async (interaction) => {
 
-  if (interaction.commandName ==="code-share") {
+	if(interaction.customId === "volume"){
     await interaction.showModal(modal);
 	}
 })
 client.on('interactionCreate', async interaction => {
-  if (interaction.type !== Discord.InteractionType.ModalSubmit) return;
-  if (interaction.customId === 'form') {
-
-
-const baslik = interaction.fields.getTextInputValue("baslik")
-const code = interaction.fields.getTextInputValue('kod')
-if (baslik > 6) return interaction.reply({content: "Başlık 6 harften uzun olamaz!", ephemeral: true})
-if (code < 20) return interaction.reply({content: "Kodun çok kısa :(", ephemeral: true})
-if (code > 300) return interaction.reply({content: "Kodun çok uzun :(", ephemeral: true})
-  if(!code.includes("const" || "let" || "var")) return interaction.reply({content: "Bu bir kod değil gibi duruyor..", ephemeral: true})
-  const kanal = "997487955860009038"
-  if (!kanal) return;
-  const embed = new Discord.EmbedBuilder()
-  .setAuthor({name: interaction.user.tag, iconURL: interaction.user.avatarURL({dynamic: true})})
-  .setDescription("Kod Başlığı: **"+baslik+"**")
-  .addFields({name: `Kod`, value: "```"+code+"```", inline: true})
-  .setTimestamp()
-  .setFooter({text: "Raven", iconURL: interaction.guild.iconURL(({}))})
-  .setColor("Green")
-  .setThumbnail(interaction.guild.iconURL({}))
-  const row = new Discord.ActionRowBuilder()
-  .addComponents(
-new Discord.ButtonBuilder()
-.setLabel("Kodu Paylaş")
-.setStyle(Discord.ButtonStyle.Success)
-.setCustomId("paylaş"),
-new Discord.ButtonBuilder()
-.setLabel("Reddet")
-.setStyle(Discord.ButtonStyle.Secondary)
-.setCustomId("reddet")
-
-  )
-  let log = "1032320545703862332"
-const as = new Discord.EmbedBuilder()
-.setDescription("<@"+interaction.user.id+">, Kodun başarıyla yetkililere incelenmek üzerine gönderildi.")
-.setColor("Grey")
-client.channels.cache.get(log).send({embeds: [as]})
-client.channels.cache.get(kanal).send({embeds: [embed], components: [row]}).then(mesaj => {
-  interaction.reply({content: "Kod yetkililere gönderildi.", ephemeral: true})
-  db.set(`kod_${mesaj.id}`, {kod: code, baslik: baslik, sahip: interaction.user.id, sahips: interaction.user.tag, avatar: interaction.user.avatarURL({dynamic: true})})
-})
+    if (interaction.type !== Discord.InteractionType.ModalSubmit) return;
+    if (interaction.customId === 'form') {
+  const string = interaction.fields.getTextInputValue('setvolume')
+  const volume = parseInt(string)
+  const queue = client.distube.getQueue(interaction);
+     if (!queue) return interaction.reply(`There is no song on the list yet.`)
+     if (isNaN(volume)) return interaction.reply("Give me number!")
+     if (volume < 1) return interaction.reply("The number must not be less than 1.")
+     if (volume > 100) return interaction.reply("The number should not be greater than 100.")
+     client.distube.setVolume(interaction, volume);
+     interaction.reply("Successfully set the volume of the music to **"+volume+"**")
 }
 })
-client.on("interactionCreate", interaction => {
-if (interaction.customId === "reddet") {
-let log = "1032320545703862332"
-const data = db.fetch(`kod_${interaction.message.id}`)
-let user = data.sahip
-const embed = new Discord.EmbedBuilder()
-.setDescription("<@"+user+">, Üzgünüm ama kodun yetkililer tarafından reddedilmiştir.")
-.setColor("Red")
-client.channels.cache.get(log).send({embeds: [embed]})
-db.delete(`kod_${interaction.message.id}`)
-}
-
-
-if (interaction.customId === "paylaş") {
-  let kategori = "1032314360971927593"
-  const data = db.fetch(`kod_${interaction.message.id}`)
-  let users = data.sahip
-  let usır = data.sahips
-  let baslik = data.baslik
-  let kod = data.kod
-  let avatar = data.avatar
-  interaction.guild.channels.create({name: baslik, type: Discord.ChannelType.GuildText}).then(kanal => {
-    kanal.setParent(kategori)
-    kanal.send({content: "```js\n"+kod+"```"})
-    kanal.send({content: "Bu kod "+usır+" tarafından paylaşılmıştır."})
-
-const embed = new Discord.EmbedBuilder()
-.setDescription("Bu kod `"+interaction.user.tag+"` tarafından kabul edilmiştir.")
-.setColor("Gold")
-interaction.update({embeds: [embed], components: []})
-let log = "1032320545703862332"
-const assd = new Discord.EmbedBuilder()
-.setDescription("<@"+users+">, Tebrikler kodun yetkililer tarafından onaylandı.")
-.setColor("Green")
-client.channels.cache.get(log).send({embeds: [assd]})
-db.delete(`kod_${interaction.message.id}`)
-})
-}
-})
+client.login(token)
